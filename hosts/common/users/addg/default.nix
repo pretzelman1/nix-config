84bb2cbb6acc203306352974c72131e60be0ec0a
addg <addgamer09@gmail.com>
2024-11-05 00:21:46 -0600

    Updates

--- hosts/common/users/addg/default.nix
+++ hosts/common/users/addg/default.nix
@@ -33,7 +33,7 @@ in {
     {
       # users.mutableUsers = false; # Only allow declarative credentials; Required for sops
       users.users.${configVars.username} = {
-        home = "/Users/${configVars.username}";
+        home = configLib.getHomeDirectory configVars.username;
         # isNormalUser = true;
         # password = "nixos"; # Overridden if sops is working
 

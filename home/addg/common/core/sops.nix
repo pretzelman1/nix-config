39f39e176de371e5e5d45d2720e531d331f99bb2
addg <addgamer09@gmail.com>
2024-11-11 22:39:54 +0800

    Updates

--- home/addg/common/core/sops.nix
+++ home/addg/common/core/sops.nix
@@ -5,17 +5,31 @@
   nix-secrets,
   ...
 }: {
-  sops.defaultSopsFile = "${nix-secrets}/secrets/secrets.yaml";
-  sops.age = {
-    sshKeyPaths = ["${configLib.getHomeDirectory config.home.username}/.ssh/id_ed25519"];
-    keyFile = "${configLib.getHomeDirectory config.home.username}/.config/sops/age/key.txt";
-    generateKey = true;
+  sops = {
+    defaultSopsFile = "${nix-secrets}/secrets/secrets.yaml";
+    age = {
+      sshKeyPaths = ["${config.home.homeDirectory}/.ssh/id_ed25519"];
+      keyFile = "${config.home.homeDirectory}/.config/sops/age/keys.txt";
+      generateKey = true;
+    };
   };
 
   sops.secrets = {
+    openai_api_key = {};
+    langchain_api_key = {};
+    tavily_api_key = {};
+    kube_config = {
+      format = "binary";
+      sopsFile = "${nix-secrets}/secrets/kube.yaml.enc";
+      path = "${config.home.homeDirectory}/.kube/config-home";
+    };
   };
 
   programs.zsh.initExtra = ''
-    export SOPS_AGE_KEY_FILE=~/.config/sops/age/key.txt
+    export SOPS_AGE_KEY_FILE=~/.config/sops/age/keys.txt
+    export OPENAI_API_KEY=$(cat ${config.sops.secrets.openai_api_key.path})
+    export LANGCHAIN_API_KEY=$(cat ${config.sops.secrets.langchain_api_key.path})
+    export TAVILY_API_KEY=$(cat ${config.sops.secrets.tavily_api_key.path})
+    export KUBECONFIG=${config.home.homeDirectory}/.kube/config:${config.sops.secrets.kube_config.path}
   '';
 }
--- hosts/common/core/sops.nix
+++ hosts/common/core/sops.nix
@@ -1,6 +1,7 @@
 
 {
   config,
+  configLib,
   nix-secrets,
   ...
 }: {
@@ -12,23 +13,5 @@
       keyFile = "/var/lib/sops-nix/key.txt";
       generateKey = true;
     };
-
-    secrets = {
-      openai_api_key = {
-        owner = "addg";
-      };
-      langchain_api_key = {
-        owner = "addg";
-      };
-      tavily_api_key = {
-        owner = "addg";
-      };
-    };
   };
-
-  programs.zsh.shellInit = ''
-    export OPENAI_API_KEY=$(cat ${config.sops.secrets.openai_api_key.path})
-    export LANGCHAIN_API_KEY=$(cat ${config.sops.secrets.langchain_api_key.path})
-    export TAVILY_API_KEY=$(cat ${config.sops.secrets.tavily_api_key.path})
-  '';
 }

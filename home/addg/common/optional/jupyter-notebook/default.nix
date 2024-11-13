d0688fd91661b5c0000a825c2854604ae80f5b1c
addg <addgamer09@gmail.com>
2024-11-13 14:28:10 +0800

    Updates

--- home/addg/common/optional/jupyter-notebook/default.nix
+++ home/addg/common/optional/jupyter-notebook/default.nix
@@ -1,9 +1,5 @@
 { pkgs, ... }: {
-  home.packages = with pkgs.python312Packages; [
-    pkgs.python312
-    ipython
-    ipykernel
-    jupyter
-    jupyterlab
+  home.packages = with pkgs; [
+    jupyter-all
   ];
 }

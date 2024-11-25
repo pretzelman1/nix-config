bfeac9d60aad98c09a4f232d77e3f38cac26bfd5
addg <addgamer09@gmail.com>
2024-11-24 21:20:25 -0600

    Updates

--- outputs/default.nix
+++ outputs/default.nix
@@ -102,27 +102,7 @@ in {
   );
 
   # Development Shells
-  devShells = forAllSystems (
-    system: let
-      pkgs = nixpkgs.legacyPackages.${system};
-    in {
-      default = pkgs.mkShell {
-        packages = with pkgs; [
-          bashInteractive
-          gcc
-          alejandra
-          deadnix
-          statix
-          typos
-          colmena
-        ];
-        name = "dots";
-        shellHook = ''
-          ${self.checks.${system}.pre-commit-check.shellHook}
-        '';
-      };
-    }
-  );
+  devShells = forAllSystems (import ./devshell.nix { inherit self nixpkgs; });
 
   # Format the nix code in this flake
   formatter = forAllSystems (
---
+++ outputs/devshell.nix
@@ -0,0 +1,20 @@
+{ self, nixpkgs, ... }:
+system: let
+  pkgs = nixpkgs.legacyPackages.${system};
+in {
+  default = pkgs.mkShell {
+    packages = with pkgs; [
+      bashInteractive
+      gcc
+      alejandra
+      deadnix
+      statix
+      typos
+      colmena
+    ];
+    name = "dots";
+    shellHook = ''
+      ${self.checks.${system}.pre-commit-check.shellHook}
+    '';
+  };
+}

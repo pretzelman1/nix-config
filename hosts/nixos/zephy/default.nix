af6f51900dbae25c9dce57d65cb51b9593052a6c
addg <addgamer09@gmail.com>
2025-01-05 19:26:27 -0600

    Updated flake

--- hosts/nixos/zephy/default.nix
+++ hosts/nixos/zephy/default.nix
@@ -72,7 +72,6 @@
     hostName = "zephy";
   };
 
-
   #   # Enable CUPS to print documents.
   # services.printing.enable = true;
 
@@ -95,51 +94,51 @@
   # needed unlock LUKS on secondary drives
   # use partition UUID
   # https://wiki.nixos.org/wiki/Full_Disk_Encryption#Unlocking_secondary_drives
-  environment.etc.crypttab.text = lib.optionalString (!config.hostSpec.isMinimal) ''
-    cryptextra UUID=d90345b2-6673-4f8e-a5ef-dc764958ea14 /luks-secondary-unlock.key
-    cryptvms UUID=ce5f47f8-d5df-4c96-b2a8-766384780a91 /luks-secondary-unlock.key
-  '';
+  # environment.etc.crypttab.text = lib.optionalString (!config.hostSpec.isMinimal) ''
+  #   cryptextra UUID=d90345b2-6673-4f8e-a5ef-dc764958ea14 /luks-secondary-unlock.key
+  #   cryptvms UUID=ce5f47f8-d5df-4c96-b2a8-766384780a91 /luks-secondary-unlock.key
+  # '';
 
   #TODO:(stylix) move this stuff to separate file but define theme itself per host
   # host-wide styling
   # stylix = {
-   #  enable = false;
-    # image = "${lib.custom.getHomeDirectory}/Downloads/Mountain Lake Painting.jpg";
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
-    # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
-    #      cursor = {
-    #        package = pkgs.foo;
-    #        name = "";
-    #      };
-    #     fonts = {
-    #monospace = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #sanSerif = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #serif = {
-    #    package = pkgs.foo;
-    #    name = "";
-    #};
-    #    sizes = {
-    #        applications = 12;
-    #        terminal = 12;
-    #        desktop = 12;
-    #        popups = 10;
-    #    };
-    #};
-   #  opacity = {
+  #  enable = false;
+  # image = "${lib.custom.getHomeDirectory}/Downloads/Mountain Lake Painting.jpg";
+  # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-hard.yaml";
+  # base16Scheme = "${pkgs.base16-schemes}/share/themes/gruvbox-dark-medium.yaml";
+  #      cursor = {
+  #        package = pkgs.foo;
+  #        name = "";
+  #      };
+  #     fonts = {
+  #monospace = {
+  #    package = pkgs.foo;
+  #    name = "";
+  #};
+  #sanSerif = {
+  #    package = pkgs.foo;
+  #    name = "";
+  #};
+  #serif = {
+  #    package = pkgs.foo;
+  #    name = "";
+  #};
+  #    sizes = {
+  #        applications = 12;
+  #        terminal = 12;
+  #        desktop = 12;
+  #        popups = 10;
+  #    };
+  #};
+  #  opacity = {
   #     applications = 1.0;
   #  terminal = 1.0;
-    #   desktop = 1.0;
-    #   popups = 0.8;
-    # };
-    # polarity = "dark";
-    # program specific exclusions
-    #targets.foo.enable = false;
+  #   desktop = 1.0;
+  #   popups = 0.8;
+  # };
+  # polarity = "dark";
+  # program specific exclusions
+  #targets.foo.enable = false;
   # };
   #hyprland border override example
   #  wayland.windowManager.hyprland.settings.general."col.active_border" = lib.mkForce "rgb(${config.stylix.base16Scheme.base0E});

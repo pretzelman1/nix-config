b5d9630467177745dc9f7236df3b827d3c9edf61
addg <addgamer09@gmail.com>
2025-03-02 18:33:37 -0600

    Updates

--- home/primary/common/core/cli/shells/bash.nix
+++ home/primary/common/core/cli/shells/bash.nix
@@ -2,14 +2,5 @@
   programs.bash = {
     enable = true;
     enableCompletion = true;
-    shellAliases = {
-      ll = "ls -alF";
-      la = "ls -A";
-      l = "ls -CF";
-
-      # Add an "alert" alias for long running commands.  Use like so:
-      #   sleep 10; alert
-      alert = "notify-send --urgency=low -i \"$([ $? = 0 ] && echo terminal || echo error)\" \"$(history|tail -n1|sed -e '\''s/^\s*[0-9]\+\s*//;s/[;&|]\s*alert$//'\'')\"";
-    };
   };
 }

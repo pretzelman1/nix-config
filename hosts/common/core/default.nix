bbdf54632d5c17ba5d02e2570d7b5c7fa48b1273
addg <addgamer09@gmail.com>
2025-03-09 23:41:45 -0500

    Updates

--- hosts/common/core/default.nix
+++ hosts/common/core/default.nix
@@ -31,64 +31,48 @@ in {
   #
   # ========== Core Host Specifications ==========
   #
-  hostSpec =
-    {
-      username = lib.mkDefault "addg";
-      handle = lib.mkDefault "addg";
+  hostSpec = {
+    username = lib.mkDefault "addg";
+    handle = lib.mkDefault "addg";
 
-      system.stateVersion = lib.mkDefault "24.11";
+    system.stateVersion = lib.mkDefault "24.11";
 
-      domain =
-        if config.hostSpec.disableSops
-        then lib.mkDefault "example.com"
-        else inputs.nix-secrets.domain;
-
-      email =
-        if config.hostSpec.disableSops
-        then
-          lib.mkDefault {
-            personal = "user@example.com";
-            work = "user@work.example.com";
-          }
-        else inputs.nix-secrets.email;
-
-      userFullName =
-        if config.hostSpec.disableSops
-        then lib.mkDefault "Example User"
-        else inputs.nix-secrets.userFullName;
-
-      githubEmail =
-        if config.hostSpec.disableSops
-        then lib.mkDefault "user@example.com"
-        else inputs.nix-secrets.githubEmail;
-
-      networking =
-        if config.hostSpec.disableSops
-        then
-          lib.mkDefault {
-            prefixLength = 24;
-            ports.tcp.ssh = 22;
-            ssh = {
-              extraConfig = "";
-            };
-          }
-        else inputs.nix-secrets.networking;
-    }
-    // (
+    domain =
       if config.hostSpec.disableSops
-      then {
-        networking = {
-          hostName = lib.mkDefault "example.com";
-          hostPlatform = lib.mkDefault "x86_64-darwin";
-        };
-      }
-      else {
-        darwin = {
-          isAarch64 = lib.mkDefault true;
-          hasPaidApps = lib.mkDefault false;
-        };
-      }
-    );
+      then lib.mkDefault "example.com"
+      else inputs.nix-secrets.domain;
+
+    email =
+      if config.hostSpec.disableSops
+      then
+        lib.mkDefault {
+          personal = "user@example.com";
+          work = "user@work.example.com";
+        }
+      else inputs.nix-secrets.email;
+
+    userFullName =
+      if config.hostSpec.disableSops
+      then lib.mkDefault "Example User"
+      else inputs.nix-secrets.userFullName;
+
+    githubEmail =
+      if config.hostSpec.disableSops
+      then lib.mkDefault "user@example.com"
+      else inputs.nix-secrets.githubEmail;
+
+    networking =
+      if config.hostSpec.disableSops
+      then
+        lib.mkDefault {
+          prefixLength = 24;
+          ports.tcp.ssh = 22;
+          ssh = {
+            extraConfig = "";
+          };
+        }
+      else inputs.nix-secrets.networking;
+  };
 
   networking.hostName = config.hostSpec.hostName;
   nixpkgs.hostPlatform = config.hostSpec.hostPlatform;
